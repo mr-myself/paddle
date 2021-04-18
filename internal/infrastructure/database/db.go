43816d47270d36cb20a9e5d925db8b6fa2305db4
Hiroki Mori <xxx.bvb.taylor@gmail.com>
2021-04-18 22:13:48 +0900

    DBコンテナ起動エラーの修正

--- internal/infrastructure/database/db.go
+++ internal/infrastructure/database/db.go
@@ -12,6 +12,6 @@ var (
 // Init is a method to initialize the database connection
 func Init() {
 	// https://qiita.com/daiki-murakami/items/c8f9df8defc937e185ee#go%E3%81%A8mysql%E3%82%92%E6%8E%A5%E7%B6%9A%E3%81%99%E3%82%8B
-	db, _ := sql.Open("mysql", "root:password@tcp(db:3306)/paddle?charset=utf8&parseTime=true&loc=Asia%2FTokyo")
+	db, _ := sql.Open("mysql", "apuser:password@tcp(db:3306)/paddle?charset=utf8&parseTime=true&loc=Asia%2FTokyo")
 	DBCon = db
 }
