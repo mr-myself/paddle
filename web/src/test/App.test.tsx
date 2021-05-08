4256c650d24e0cc536f77f75adf67dc0333afe85
mr-myself <keisuke@mr-myself.com>
2021-05-08 22:05:39 +0900

    added atoms tests

--- web/src/test/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import FeedsPage from '../containers/pages/FeedsPage'
-
-test('renders learn react link', () => {
-  render(<FeedsPage />)
-  const linkElement = screen.getByText(/test/i)
-  expect(linkElement).toBeInTheDocument()
-})
