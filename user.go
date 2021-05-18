d64e71b5716a2646167b877256136a5c1e43ef14
Mod12d <terumi8.neu@icloud.com>
2021-05-19 07:37:37 +0900

    [ch384] User.goの修正

--- user.go
+++ user.go
@@ -1,46 +1,18 @@
-package main
+type User struct {
+	//ORMの確認
+	//gorm.Model
+    Email string `form:"email" binding:"required" gorm:"unique;not null"`
+    Password string `form:"password" binding:"required"`
+}
 
-import (
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
+func createUser(email string, password string) []error {
+    passwordEncrypt, _ := crypto.PasswordEncrypt(password)
+    //db := gormConnect()
+    defer db.Close()
+    // Insert処理
+    if err := db.Create(&User{Email: email, Password: passwordEncrypt}).GetErrors(); err != nil {
+        return err
+    }
+    return nil
 
-	"github.com/ChubachiPT21/paddle/internal/infrastructure/database"
-	"github.com/ChubachiPT21/paddle/internal/infrastructure/repository"
-	"github.com/ChubachiPT21/paddle/internal/routes/paddle"
-	"github.com/ChubachiPT21/paddle/pkg/routes"
-	_ "github.com/go-sql-driver/mysql"
-)
-
-func main() {
-	database.Init()
-
-	r := gin.New()
-	r.Use(cors.Default())
-
-	v1 := r.Group("/v1")
-	routes.AddRoutes(
-		v1,
-		paddle.GetFeeds(repository.NewFeedRepository())...,
-	)
-	routes.AddRoutes(
-		v1,
-		paddle.GetPreview()...,
-	)
-	routes.AddRoutes(
-		v1,
-		paddle.GetSources(repository.NewSourceRepository())...,
-	)
-	routes.AddRoutes(
-		v1,
-		paddle.CreateSource()...,
-	)
-	routes.AddRoutes(
-		v1,
-		paddle.CreateFeeds()...,
-	)
-	routes.AddRoutes(
-		v1,
-		paddle.CreateInterest()...,
-	)
-	r.Run(":10330") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
