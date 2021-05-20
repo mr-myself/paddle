5901a6e775f5cc4bd49100cb901338a2e1e9193d
Mod12d <terumi8.neu@icloud.com>
2021-05-20 22:39:42 +0900

    ch384 revert

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ChubachiPT21/paddle/pkg/routes"
 	_ "github.com/go-sql-driver/mysql"
 
+
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/ChubachiPT21/paddle/user"
@@ -48,30 +49,6 @@ func main() {
 		v1,
 		paddle.CreateInterest()...,
 	)
-	//ユーザ情報登録
-    r. POST("/signup", func(c *gin.Context)  {
-		r.Use(sessions.Sessions("mysession", store))
-		var form User
-		email := c.PostForm("email")
 
-		//TODO DBのパスワードと突合処理
-		password := c.PostForm("password")
-		passwordEncrypt, _ := crypto.PasswordEncrypt(password)
-
-		User.createUser(email,password)
-
-		//TODO nilチェック 等
-		//TODO セッション格納する際のフォーマット
-		session.Set("Email", email)
-		session.Set("Password", password)
-		session.Save()
-		//ステータスコード返却
-		c.Status(http.StatusOK)
-				
-		// TODO バリデーションチェック
-		// TODO 重複チェック
-		// TODO 認証後限定の機能
-
-    })
 	r.Run(":10330") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
